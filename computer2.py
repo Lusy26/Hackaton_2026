a56c72b0bbbe7c672d12f507dde80fd1fa4229df
Ziyang_bNotebook <260989378+ziyang-dev@users.noreply.github.com>
2026-06-13 12:38:17 +0200

    6

--- computer2.py
+++ computer2.py
@@ -1,18 +1,49 @@
 import socket
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+import threading
 
-HOST = "10.10.11.204"
+# --------- SOCKET ---------
+HOST = "IP_DEL_SERVIDOR"
 PORT = 12345
 
 client = socket.socket()
 client.connect((HOST, PORT))
 
-print("Conectado al servidor")
+# --------- VENTANA ---------
+ventana = tk.Tk()
+ventana.title("Chat")
 
-while True:
-    msg = input("Tú: ")
+chat = ScrolledText(ventana, width=50, height=20)
+chat.pack()
 
+entrada = tk.Entry(ventana, width=40)
+entrada.pack()
+
+# --------- RECIBIR MENSAJES ---------
+def recibir():
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            chat.insert(tk.END, "Servidor: " + msg + "\n")
+        except:
+            break
+
+threading.Thread(target=recibir, daemon=True).start()
+
+# --------- ENVIAR MENSAJES ---------
+def enviar(event=None):
+    msg = entrada.get()
     client.send(msg.encode())
 
-    respuesta = client.recv(1024).decode()
+    chat.insert(tk.END, "Tú: " + msg + "\n")
+    entrada.delete(0, tk.END)
 
-    print("Servidor:", respuesta)
+entrada.bind("<Return>", enviar)
+
+boton = tk.Button(ventana, text="Enviar", command=enviar)
+boton.pack()
+
+ventana.mainloop()
+
+client.close()
